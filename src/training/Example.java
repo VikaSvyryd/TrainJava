fee645afa21f76ea3c8c328e531d71a4c6514d8b
VikaSvyryd <svyryd.vika@gmail.com>
2016-02-12 10:08:25 +0200

    Initial commit

---
+++ src/training/Example.java
@@ -0,0 +1,9 @@
+package training;
+    /*This is the simplest Java program. Let's give it the name "Example.java"*/
+
+public class Example {
+    //The program begins from the main method//
+    public static void main (String args[]) {
+        System.out.println("This is the simplest Java-program");
+    }
+}
