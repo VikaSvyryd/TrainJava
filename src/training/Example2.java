9f79a7a940e0b9a2128d5d915cccbe5696a3e552
VikaSvyryd <svyryd.vika@gmail.com>
2016-02-12 10:18:57 +0200

    Commited First.java and Example2.java

---
+++ src/training/Example2.java
@@ -0,0 +1,13 @@
+package training;
+/*This is another example of the simplest Java-program. Let's give it a name "Example2"*/
+
+public class Example2 {
+    public static void main (String args []) {
+        int num; //This is the var. num
+        num=100; //Var. num equals 100
+        System.out.println ("This is var.num:" + num);
+        num=num*2;
+        System.out.print ("The var.num*2 equals:");
+        System.out.println (num);
+    }
+}
