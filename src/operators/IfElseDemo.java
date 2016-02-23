e961ab4e78c4641b6e56cb6acb977f8a1e5f1dc0
VikaSvyryd <svyryd.vika@gmail.com>
2016-02-23 15:58:58 +0200

    Began new chapter, added new javs files

---
+++ src/operators/IfElseDemo.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class IfElseDemo {
+    public static void main (String args []) {
+        int month = 10; //...
+        String season;
+
+        if(month == 12 || month == 1 || month == 2)
+            season = "winter";
+        else if(month ==3 || month == 4 || month == 5)
+            season = "spring";
+        else if(month == 6 || month == 7 || month == 8)
+            season = "summer";
+        else if(month == 9 || month == 10 || month == 11)
+            season = "autmn";
+        else
+            season = "imagined month";
+        System.out.println("April is " + season + " month.");
+    }
+}
