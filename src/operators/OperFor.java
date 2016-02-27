b73738bf15724d8a866816fadc5897d3107a2d9e
VikaSvyryd <svyryd.vika@gmail.com>
2016-02-27 08:12:20 +0200

    Added new java operator files

---
+++ src/operators/OperFor.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class OperFor {
+    public static void main(String [] args){
+
+        for (int n = 10; n > 0; n--)
+        {
+            System.out.println(n);
+        }
+
+//...
+        
+
+
+
+
+
+    }
+}
